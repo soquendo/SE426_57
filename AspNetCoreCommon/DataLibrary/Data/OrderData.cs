500f8e957ad6790ccad52534d025c4980a1b65e7
Sebastian <soquendo@email.neit.edu>
2023-10-19 16:35:28 -0400

    mix

--- AspNetCoreCommon/DataLibrary/Data/OrderData.cs
+++ AspNetCoreCommon/DataLibrary/Data/OrderData.cs
@@ -20,6 +20,21 @@ namespace DataLibrary.Data
             _connectionString = connectionString;
         }
 
-        //slide 22
+        public async Task<int> CreateOrder(OrderModel order)
+        {
+            DynamicParameters p = new DynamicParameters();  //dapper version of parameters
+
+            p.Add("OrderName", order.OrderName);    //Most of these are just specifying the params and the values we fill them with
+            p.Add("OrderDate", order.OrderDate);    //Param Direction is Input by default, so we not need to specify here
+            p.Add("FoodId", order.FoodId);
+            p.Add("Quantity", order.Quantity);
+            p.Add("Total", order.Total);
+            p.Add("Id", DbType.Int32, direction: ParameterDirection.Output);    //this is a value we will receive after data is stored (the new Id assigned)
+
+            //perform the actual insert
+            await _dataAccess.SaveData("dbo.spOrders_Insert", p, _connectionString.SqlConnectionName);
+
+            return p.Get<int>("Id"); //return to new Id created for the new record that we get from the Scope_Identity command in the stored proc we called
+        }
     }
 }
